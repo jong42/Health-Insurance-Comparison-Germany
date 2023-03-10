f90dffa9a14931ff12db4148c413a4a3a402ef6e
Jonas <jonasguetter@gmx.de>
2023-03-10 17:53:07 +0100

    Add states bar plot

--- hic_ger/code/data_analysis/app.py
+++ hic_ger/code/data_analysis/app.py
@@ -3,14 +3,21 @@ from dash import Dash, dcc, html
 import plotly.express as px
 
 
-data_path = "../../data/data_cleaned.csv"
-df = pd.read_csv(data_path)
+providers_path = "../../data/providers_cleaned.csv"
+states_path = "../../data/states_cleaned.csv"
+df_prov = pd.read_csv(providers_path)
+df_states = pd.read_csv(states_path)
 
 # Create bar plots
-fig = px.bar(df, 'fee', 'name')
+fig = px.bar(df_prov, 'fee', 'name')
 bar_plot_fees = dcc.Graph(id='bar plot fees', figure=fig)
-fig = px.bar(df, 'services_count', 'name')
+fig = px.bar(df_prov, 'services_count', 'name')
 bar_plot_services = dcc.Graph(id='bar plot services', figure=fig)
+fig = px.bar(df_states,'provider_count', 'Unnamed: 0')
+bar_plot_states = dcc.Graph(id='bar plot states', figure=fig)
+
+#Create maps
+pass
 
 
 # Initialise the app
@@ -22,7 +29,8 @@ app.layout = html.Div(children=[
     html.Div('Text Information', className='row'),
     html.Div(className='column', children=[
         html.Div(bar_plot_fees),
-        html.Div(bar_plot_services)
+        html.Div(bar_plot_services),
+        html.Div(bar_plot_states)
     ]),
     html.Div('Graph area 2', className='column'),
     html.Div('Graph area 3', className='column')
