ac03cfacbfabdd05d8fc23e18f92665b2807c176
Jonas <jonasguetter@gmx.de>
2023-03-13 11:40:44 +0100

    Include first draft for map plots

--- hic_ger/code/data_analysis/app.py
+++ hic_ger/code/data_analysis/app.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from dash import Dash, dcc, html
 import plotly.express as px
+import json
 
 
 providers_path = "../../data/providers_cleaned.csv"
 states_path = "../../data/states_cleaned.csv"
+states_map_path = "../../data/german_states.geo.json"
 df_prov = pd.read_csv(providers_path)
 df_states = pd.read_csv(states_path)
 
@@ -17,7 +19,42 @@ fig = px.bar(df_states,'provider_count', 'Unnamed: 0')
 bar_plot_states = dcc.Graph(id='bar plot states', figure=fig)
 
 #Create maps
-pass
+with open(states_map_path) as f:
+    geojson_states = json.load(f)
+
+fig_fees = px.choropleth_mapbox(df_states,
+                           geojson=geojson_states,
+                           color='avg_fee',
+                           featureidkey='properties.name',
+                           locations='Unnamed: 0',
+                           center={"lat": 51.1656, "lon": 10.4515},
+                           zoom=4,
+                           mapbox_style="white-bg"
+                           )
+fig_services = px.choropleth_mapbox(df_states,
+                           geojson=geojson_states,
+                           color='avg_services_count',
+                           featureidkey='properties.name',
+                           locations='Unnamed: 0',
+                           center={"lat": 51.1656, "lon": 10.4515},
+                           zoom=4,
+                           mapbox_style="white-bg"
+                           )
+fig_providers = px.choropleth_mapbox(df_states,
+                           geojson=geojson_states,
+                           color='provider_count',
+                           featureidkey='properties.name',
+                           locations='Unnamed: 0',
+                           center={"lat": 51.1656, "lon": 10.4515},
+                           zoom=4,
+                           mapbox_style="white-bg"
+                           )
+
+map_plot_fees = dcc.Graph(id='map plot fees', figure=fig_fees)
+map_plot_services = dcc.Graph(id='map plot services', figure=fig_services)
+map_plot_providers = dcc.Graph(id='map plot providers', figure=fig_providers)
+
+
 
 
 # Initialise the app
@@ -32,7 +69,11 @@ app.layout = html.Div(children=[
         html.Div(bar_plot_services),
         html.Div(bar_plot_states)
     ]),
-    html.Div('Graph area 2', className='column'),
+    html.Div(className='column', children=[
+        html.Div(map_plot_fees),
+        html.Div(map_plot_services),
+        html.Div(map_plot_providers)
+    ]),
     html.Div('Graph area 3', className='column')
 ])
 
