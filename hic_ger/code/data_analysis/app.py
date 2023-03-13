07592149b903738d4d84af52e9f785cf0267e8a4
Jonas <jonasguetter@gmx.de>
2023-03-13 11:54:15 +0100

    Include first draft for scatter plots

--- hic_ger/code/data_analysis/app.py
+++ hic_ger/code/data_analysis/app.py
@@ -3,7 +3,6 @@ from dash import Dash, dcc, html
 import plotly.express as px
 import json
 
-
 providers_path = "../../data/providers_cleaned.csv"
 states_path = "../../data/states_cleaned.csv"
 states_map_path = "../../data/german_states.geo.json"
@@ -15,47 +14,50 @@ fig = px.bar(df_prov, 'fee', 'name')
 bar_plot_fees = dcc.Graph(id='bar plot fees', figure=fig)
 fig = px.bar(df_prov, 'services_count', 'name')
 bar_plot_services = dcc.Graph(id='bar plot services', figure=fig)
-fig = px.bar(df_states,'provider_count', 'Unnamed: 0')
+fig = px.bar(df_states, 'provider_count', 'Unnamed: 0')
 bar_plot_states = dcc.Graph(id='bar plot states', figure=fig)
 
-#Create maps
+# Create maps
 with open(states_map_path) as f:
     geojson_states = json.load(f)
 
 fig_fees = px.choropleth_mapbox(df_states,
-                           geojson=geojson_states,
-                           color='avg_fee',
-                           featureidkey='properties.name',
-                           locations='Unnamed: 0',
-                           center={"lat": 51.1656, "lon": 10.4515},
-                           zoom=4,
-                           mapbox_style="white-bg"
-                           )
+                                geojson=geojson_states,
+                                color='avg_fee',
+                                featureidkey='properties.name',
+                                locations='Unnamed: 0',
+                                center={"lat": 51.1656, "lon": 10.4515},
+                                zoom=4,
+                                mapbox_style="white-bg"
+                                )
 fig_services = px.choropleth_mapbox(df_states,
-                           geojson=geojson_states,
-                           color='avg_services_count',
-                           featureidkey='properties.name',
-                           locations='Unnamed: 0',
-                           center={"lat": 51.1656, "lon": 10.4515},
-                           zoom=4,
-                           mapbox_style="white-bg"
-                           )
+                                    geojson=geojson_states,
+                                    color='avg_services_count',
+                                    featureidkey='properties.name',
+                                    locations='Unnamed: 0',
+                                    center={"lat": 51.1656, "lon": 10.4515},
+                                    zoom=4,
+                                    mapbox_style="white-bg"
+                                    )
 fig_providers = px.choropleth_mapbox(df_states,
-                           geojson=geojson_states,
-                           color='provider_count',
-                           featureidkey='properties.name',
-                           locations='Unnamed: 0',
-                           center={"lat": 51.1656, "lon": 10.4515},
-                           zoom=4,
-                           mapbox_style="white-bg"
-                           )
+                                     geojson=geojson_states,
+                                     color='provider_count',
+                                     featureidkey='properties.name',
+                                     locations='Unnamed: 0',
+                                     center={"lat": 51.1656, "lon": 10.4515},
+                                     zoom=4,
+                                     mapbox_style="white-bg"
+                                     )
 
 map_plot_fees = dcc.Graph(id='map plot fees', figure=fig_fees)
 map_plot_services = dcc.Graph(id='map plot services', figure=fig_services)
 map_plot_providers = dcc.Graph(id='map plot providers', figure=fig_providers)
 
-
-
+#Create scatter plots
+fig_prov = px.scatter(df_prov, x='fee', y='services_count')
+fig_states = px.scatter(df_states, x='avg_fee', y='avg_services_count')
+scatter_plot_prov = dcc.Graph(id='scatter plot prov', figure=fig_prov)
+scatter_plot_states = dcc.Graph(id='scatter plot states', figure=fig_states)
 
 # Initialise the app
 app = Dash(__name__)
@@ -74,7 +76,10 @@ app.layout = html.Div(children=[
         html.Div(map_plot_services),
         html.Div(map_plot_providers)
     ]),
-    html.Div('Graph area 3', className='column')
+    html.Div(className='column', children=[
+        html.Div(scatter_plot_prov),
+        html.Div(scatter_plot_states)
+    ])
 ])
 
 # Run the app
